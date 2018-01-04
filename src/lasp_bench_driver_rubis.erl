8beb4531e5eefbc2b385efd4068977585bed8684
ergl <ergl@users.noreply.github.com>
2018-01-04 16:51:59 +0100

    Clean up rubis driver

--- src/lasp_bench_driver_rubis.erl
+++ src/lasp_bench_driver_rubis.erl
@@ -5,12 +5,13 @@
 
 -include("lasp_bench.hrl").
 
--define(TTABLE, lasp_bench_driver_rubis_transition_table).
+-define(RUBIS_CORE, lasp_bench_driver_rubis_core).
 
 -record(state, {
     server_ip,
     server_port,
-    http_connection
+    http_connection,
+    rubis_state
 }).
 
 new(_Id) ->
@@ -19,31 +20,66 @@ new(_Id) ->
     Ip = lasp_bench_config:get(rubis_ip, "127.0.0.1"),
     Port = lasp_bench_config:get(rubis_port, 5000),
     {ok, ConnRef} = hackney:connect(hackney_tcp, Ip, Port, []),
+
     {ok, #state{
         server_ip = Ip,
         server_port = integer_to_list(Port),
-        http_connection = ConnRef
+        http_connection = ConnRef,
+        rubis_state = ?RUBIS_CORE:new_rubis_state()
     }}.
 
-run(registeruser, KeyGen, _, State) ->
-    %% TODO
-    Key = KeyGen(),
+run(registeruser, _, _, State) ->
+    Region = ?RUBIS_CORE:random_region(),
+    {{Username, Password}, NewRubisState} = ?RUBIS_CORE:gen_new_user(State#state.rubis_state),
+    Payload = jsx:encode([Username, Password, {region, Region}]),
+    build_request(
+        post,
+        {"/users", Payload},
+        fun create_user_handler/2,
+        State#state{rubis_state = NewRubisState}
+    );
 
-    BinaryRawId = integer_to_binary(Key),
-    BinaryRegionId = integer_to_binary((Key rem 5) + 1),
+run(browsecategories, _, _, State) ->
+    build_request(get, "/categories", State);
 
-    Region = <<"REGION_", BinaryRegionId/binary>>,
-    UserName = <<"randomUsernameNumber", BinaryRawId/binary>>,
-    User = [{name, UserName}, {password, <<"This is a password">>}, {region, Region}],
-    build_request(post, {"/register", jsx:encode(User)}, State);
+run(browseregions, _, _, State) ->
+    build_request(get, "/regions", State);
+
+run(searchitemsincategory, _, _, State) ->
+    RegionId = ?RUBIS_CORE:random_region_id(),
+    URL = "/searchByCategory/" ++ integer_to_list(RegionId),
+    build_request(get, URL, State);
+
+run(searchitemsinregion, _, _, State) ->
+    RegionId = ?RUBIS_CORE:random_region_id(),
+    CategoryId = ?RUBIS_CORE:random_category_id(),
+    Payload = jsx:encode([{regionId, RegionId}, {categoryId, CategoryId}]),
+    build_request(post, {"/searchByRegion", Payload}, State);
+
+run(viewitem, _, _, State) ->
+    ItemId = ?RUBIS_CORE:random_item_id(),
+    URL = "/items/" ++ integer_to_list(ItemId),
+    build_request(get, URL, State);
+
+run(viewuserinfo, _, _, State) ->
+    UserId = ?RUBIS_CORE:random_user_id(State#state.rubis_state),
+    URL = "/users/" ++ integer_to_list(UserId),
+    build_request(get, URL, State);
+
+run(viewbidhistory, _, _, State) ->
+    ItemId = ?RUBIS_CORE:random_item_id(),
+    URL = "/items/" ++ integer_to_list(ItemId) ++ "/bids",
+    build_request(get, URL, State);
 
 run(_, _, _, State) ->
-    timer:sleep(1000),
     {ok, State}.
 
 %% Networking Utils
 
-build_request(get, Path, State=#state{
+build_request(Verb, Path, State) ->
+    build_request(Verb, Path, fun simple_resp_handler/2, State).
+
+build_request(get, Path, Handler, State=#state{
     server_ip=Ip,
     server_port=Port,
     http_connection=Conn
@@ -52,9 +88,9 @@ build_request(get, Path, State=#state{
     Headers = [{<<"Connection">>, <<"keep-alive">>}],
     Payload = <<>>,
     Req = {get, URL, Headers, Payload},
-    perf_req(Conn, Req, State);
+    perf_req(Conn, Req, State, Handler);
 
-build_request(post, {Path, Payload}, State=#state{
+build_request(post, {Path, Payload}, Handler, State=#state{
     server_ip=Ip,
     server_port=Port,
     http_connection=Conn
@@ -65,13 +101,10 @@ build_request(post, {Path, Payload}, State=#state{
         {<<"Content-Type">>, <<"application/json">>}
     ],
     Req = {post, URL, Headers, Payload},
-    perf_req(Conn, Req, State).
+    perf_req(Conn, Req, State, Handler).
 
 generate_url(Address,Port,Path) ->
     list_to_binary("http://" ++ Address ++ ":" ++ Port ++ Path).
-
-perf_req(Connection, Req, State) ->
-    perf_req(Connection, Req, State, fun simple_resp_handler/2).
 
 perf_req(_Conn, Req, State, Handler) ->
     {Method, URL, Headers, Payload} = Req,
@@ -88,3 +121,10 @@ perf_req(_Conn, Req, State, Handler) ->
 
 simple_resp_handler(_Body, State) ->
     {ok, State}.
+
+create_user_handler(Body, State = #state{rubis_state=RubisState}) ->
+    #{ result :=
+        #{generatedKeyValue :=
+            #{ pkValue := NewId } } } = jsx:decode(Body, [{labels, atom}, return_maps]),
+    NewRubisState = ?RUBIS_CORE:update_state({user_id, NewId}, RubisState),
+    {ok, State#state{rubis_state=NewRubisState}}.
---
+++ src/lasp_bench_driver_rubis_core.erl
@@ -0,0 +1,165 @@
+-module(lasp_bench_driver_rubis_core).
+
+-define(RUBIS_CATEGORIES, 20).
+-define(RUBIS_ITEMS, 727).
+-define(RUBIS_REGIONS, [
+    <<"AZ--Phoenix">>,
+    <<"CA--Los Angeles">>,
+    <<"CA--Oakland">>,
+    <<"CA--Sacramento">>,
+    <<"CA--San Diego">>,
+    <<"CA--San Francisco">>,
+    <<"CA--San Jose">>,
+    <<"CO--Denver">>,
+    <<"CT--Hartford">>,
+    <<"DC--Washington">>,
+    <<"FL--Jacksonville">>,
+    <<"FL--Miami">>,
+    <<"FL--Orlando">>,
+    <<"FL--Tampa-St. Pete">>,
+    <<"FL--West Palm Beach">>,
+    <<"GA--Atlanta">>,
+    <<"HI--Honolulu">>,
+    <<"ID--Billings-Boise">>,
+    <<"IL--Chicago">>,
+    <<"IN--Indianapolis">>,
+    <<"KS--Kansas City">>,
+    <<"KY--Louisville">>,
+    <<"LA--New Orleans">>,
+    <<"MA--Boston ">>,
+    <<"MD--Baltimore">>,
+    <<"MI--Detroit">>,
+    <<"MI--Grand Rapids">>,
+    <<"MN--Minn-St. Paul">>,
+    <<"MO--Kansas City">>,
+    <<"MO--St. Louis">>,
+    <<"MT--Billings-Boise">>,
+    <<"NC--Charlotte">>,
+    <<"NC--Greensboro">>,
+    <<"NC--Raleigh-Durham">>,
+    <<"ND--Bismarck-Pierre">>,
+    <<"NM--Albuquerque">>,
+    <<"NV--Las Vegas">>,
+    <<"NY--Albany">>,
+    <<"NY--Buffalo">>,
+    <<"NY--New York">>,
+    <<"NY--Rochester">>,
+    <<"OH--Cincinnati">>,
+    <<"OH--Cleveland">>,
+    <<"OH--Columbus">>,
+    <<"OH--Dayton">>,
+    <<"OK--Oklahoma City">>,
+    <<"OR--Portland">>,
+    <<"PA--Philadelphia">>,
+    <<"PA--Pittsburgh">>,
+    <<"RI--Providence">>,
+    <<"SD--Bismarck-Pierre">>,
+    <<"TN--Memphis">>,
+    <<"TN--Nashville">>,
+    <<"TX--Austin">>,
+    <<"TX--Dallas-Fort Worth">>,
+    <<"TX--Houston">>,
+    <<"TX--San Antonio">>,
+    <<"UT--Salt Lake City">>,
+    <<"VA--Norfolk-VA Beach">>,
+    <<"VA--Richmond">>,
+    <<"WA--Seattle-Tacoma">>,
+    <<"WI--Milwaukee">>
+]).
+
+-type username() :: bitstring().
+-type user() :: {{atom(), bitstring()}, {atom(), bitstring()}}.
+
+-record(rubis_state, {
+    max_seen_user_id :: non_neg_integer() | not_set,
+    own_user_ids_start_at :: non_neg_integer() | not_set,
+    generated_usernames :: list(username()),
+
+    max_seen_item_id :: non_neg_integer() | not_set,
+    own_item_ids_start_at :: non_neg_integer() | not_set
+}).
+
+-type rubis_state() :: #rubis_state{}.
+
+-export([new_rubis_state/0,
+         update_state/2,
+         gen_new_user/1,
+         random_user/1,
+         random_user_id/1,
+         random_region/0,
+         random_item_id/1,
+         random_region_id/0,
+         random_category_id/0]).
+
+-spec new_rubis_state() -> rubis_state().
+new_rubis_state() ->
+    #rubis_state{
+        max_seen_user_id = not_set,
+        own_user_ids_start_at = not_set,
+        generated_usernames = [],
+
+        max_seen_item_id = not_set,
+        own_item_ids_start_at = not_set
+    }.
+
+-spec update_state({atom(), non_neg_integer()}, rubis_state()) -> rubis_state().
+update_state({user_id, N}, State = #rubis_state{max_seen_user_id = not_set, own_user_ids_start_at = not_set}) ->
+    State#rubis_state{max_seen_user_id=N,own_user_ids_start_at=N};
+
+update_state({user_id, N}, State = #rubis_state{max_seen_user_id = Max, own_user_ids_start_at = Start}) ->
+    NewMax = case N > Max of
+        true -> N;
+        false -> Max
+     end,
+    NewStart = case N < Start of
+        true -> N;
+        false -> Start
+    end,
+    State#rubis_state{max_seen_user_id = NewMax, own_user_ids_start_at = NewStart}.
+
+-spec gen_new_user(rubis_state()) -> {user(), rubis_state()}.
+gen_new_user(State = #rubis_state{generated_usernames = Usernames}) ->
+    Username = random_string(24),
+    User = {{username, Username}, {password, Username}},
+    {User, State#rubis_state{generated_usernames = [Username | Usernames]}}.
+
+-spec random_user_id(rubis_state()) -> non_neg_integer().
+random_user_id(#rubis_state{max_seen_user_id = not_set}) ->
+    1;
+
+random_user_id(#rubis_state{max_seen_user_id = Max}) ->
+    crypto:rand_uniform(1, Max + 1).
+
+-spec random_user(rubis_state()) -> user().
+random_user(State = #rubis_state{own_user_ids_start_at = N, generated_usernames = Usernames}) ->
+    ChosendId = random_user_id(State),
+    case ChosendId < N of
+        true ->
+            IdStr = integer_to_binary(ChosendId),
+            {{username, <<"user_", IdStr/binary>>}, {password, {<<"password_", IdStr/binary>>}}};
+        false ->
+            Username = lists:nth((ChosendId - N), Usernames),
+            {{username, Username}, {password, Username}}
+    end.
+
+-spec random_region_id() -> non_neg_integer().
+random_region_id() ->
+    crypto:rand_uniform(1, length(?RUBIS_REGIONS)).
+
+-spec random_region() -> bitstring().
+random_region() ->
+    lists:nth(random_region_id(), ?RUBIS_REGIONS).
+
+-spec random_category_id() -> non_neg_integer().
+random_category_id() ->
+    crypto:rand_uniform(1, ?RUBIS_CATEGORIES + 1).
+
+-spec random_item_id(rubis_state()) -> non_neg_integer().
+random_item_id(#rubis_state{max_seen_item_id = not_set}) ->
+    1;
+
+random_item_id(#rubis_state{max_seen_item_id = Max}) ->
+    crypto:rand_uniform(1, Max + 1).
+
+random_string(Lenght) ->
+    base64:encode(crypto:strong_rand_bytes(Lenght)).
