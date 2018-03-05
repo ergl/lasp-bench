8a2a5f90a9b0b18273ac93920f4e8a2dacfb420e
ergl <ergl@users.noreply.github.com>
2018-03-05 15:28:59 +0100

    Pass node info to genbench

--- rubis-generate.sh
+++ rubis-generate.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 generate() {
-  local out_file="$(pwd)/${2}"
+  local host="${1}"
+  local port="${2}"
+  local table_file="$(pwd)"/scripts/tables/"${3}"
+  local out_file="$(pwd)/${4}"
   pushd scripts/
   local load_file="$(pwd)"/output.json
   if [[ -a "${load_file}" ]]; then
-    ./genbenchrun.escript "$(pwd)"/tables/"${1}" "$(pwd)"/output.json > "${out_file}"
+    ./genbenchrun.escript "${host}" "${port}" "${table_file}" "$(pwd)"/output.json > "${out_file}"
   else
     popd
     exit 1
