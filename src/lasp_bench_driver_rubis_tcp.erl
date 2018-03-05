66565a20fcfc0b09dbc546b1822d7f38021752f4
ergl <ergl@users.noreply.github.com>
2018-03-05 16:49:44 +0100

    Use pb for benchmark

--- src/lasp_bench_driver_rubis_tcp.erl
+++ src/lasp_bench_driver_rubis_tcp.erl
@@ -5,33 +5,160 @@
 
 -include("lasp_bench.hrl").
 
+-define(core, lasp_bench_driver_rubis_core).
+
 -record(state, {
     %% Antidote connection to the remote node
     worker_id,
-    target_ip,
-    target_port,
-    socket
+    socket,
+    rubis_state
 }).
 
 new(Id) ->
-    Ip = lasp_bench_config:get(pb_ip),
-    Port = lasp_bench_config:get(pb_port),
+    _ = application:ensure_all_started(rubis_proto),
+
+    Ip = lasp_bench_config:get(rubis_ip, '127.0.0.1'),
+    Port = lasp_bench_config:get(rubis_port, 7878),
     Options = [binary, {active, false}, {packet, 2}],
     {ok, Sock} = gen_tcp:connect(Ip, Port, Options),
-    {ok, #state{worker_id=Id,
-                target_ip=Ip,
-                socket=Sock,
-                target_port=Port}}.
 
-run(send, _, ValueGen, State = #state{socket=Sock}) ->
-    NewVal = ValueGen(),
-    gen_tcp:send(Sock, NewVal),
-    RecVal = gen_tcp:recv(Sock, 0),
-    case RecVal of
-        {ok, NewVal} ->
+    RubisState = lasp_bench_driver_rubis_core:new_rubis_state(),
+    {ok, #state{socket=Sock,
+                worker_id=Id,
+                rubis_state=RubisState}}.
+
+run(registeruser, _, _, State = #state{socket=Sock, rubis_state=RState}) ->
+    RegionId = ?core:random_region_id(RState),
+    {{Username, Password}, NewState} = ?core:gen_new_user(RState),
+    Msg = rubis_proto:register_user(Username, Password, RegionId),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('RegisterUser', BinReply),
+    case Resp of
+        {ok, Id} ->
+            FinalState = ?core:update_state({confirm_user, Id, Username}, NewState),
+            {ok, State#state{rubis_state=FinalState}};
+
+         {error, Reason} ->
+             FinalState = ?core:update_state({discard_user, Username}, NewState),
+             {error, Reason, State#state{rubis_state=FinalState}}
+    end;
+
+run(browsecategories, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(searchitemsincategory, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(browseregions, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(searchitemsinregion, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(viewitem, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(viewuserinfo, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(viewbidhistory, _, _, State) ->
+    %% TODO(borja)
+    {ok, State};
+
+run(buynowauth, _, _, State) ->
+    perform_auth(State);
+
+run(storebuynow, _, _, State = #state{socket=Sock,rubis_state=RState}) ->
+    {ItemId, UserId, Q} = ?core:gen_buy_now(RState),
+    Msg = rubis_proto:store_buy_now(ItemId, UserId, Q),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('StoreBuyNow', BinReply),
+    case Resp of
+        {ok, _} ->
             {ok, State};
-        {ok, _Other} ->
-            {error, no_match, State};
+
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+
+run(putbidauth, _, _, State) ->
+    perform_auth(State);
+
+run(storebid, _, _, State = #state{socket=Sock,rubis_state=RState}) ->
+    {ItemId, UserId, Price} = ?core:gen_bid(RState),
+    Msg = rubis_proto:store_bid(ItemId, UserId, Price),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('StoreBid', BinReply),
+    case Resp of
+        {ok, _} ->
+            {ok, State};
+
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+
+run(putcommentauth, _, _, State) ->
+    perform_auth(State);
+
+run(storecomment, _, _, State = #state{socket=Sock,rubis_state=RState}) ->
+    {ItemId, FromId, ToId, Rating, Body} = ?core:gen_comment(RState),
+    Msg = rubis_proto:store_comment(ItemId, FromId, ToId, Rating, Body),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('StoreComment', BinReply),
+    case Resp of
+        {ok, _} ->
+            {ok, State};
+
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+
+run(registeritem, _, _, State = #state{socket=Sock, rubis_state=RState}) ->
+    {ItemName, Description, Quantity, CategoryId, SellerId} = ?core:gen_item(RState),
+    Msg = rubis_proto:store_item(ItemName, Description, Quantity, CategoryId, SellerId),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('StoreItem', BinReply),
+    case Resp of
+        {ok, ItemId} ->
+            NewState = ?core:update_state({confirm_item, ItemId}, RState),
+            {ok, State#state{rubis_state=NewState}};
+
+        {error, Reason} ->
+            {error, Reason, State}
+    end;
+
+run(aboutme_auth, _, _, State) ->
+    perform_auth(State);
+
+run(aboutme, _, _, State) ->
+    %% TODO(borja)
+    {ok, State}.
+
+
+%% Util functions
+
+perform_auth(State = #state{socket=Sock, rubis_state=RState}) ->
+    {Username, Pass} = ?core:random_user(RState),
+    Msg = rubis_proto:auth_user(Username, Pass),
+    ok = gen_tcp:send(Sock, Msg),
+    {ok, BinReply} = gen_tcp:recv(Sock, 0),
+    Resp = rubis_proto:decode_reply('AuthUser', BinReply),
+    case Resp of
+        {ok, Id} ->
+            NewState = ?core:set_logged_in(Id, RState),
+            {ok, State#state{rubis_state=NewState}};
+
         {error, Reason} ->
             {error, Reason, State}
     end.
