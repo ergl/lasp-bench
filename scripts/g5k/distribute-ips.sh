a6c0118ff1d3c470309832e309ae1ae130b9eabf
ergl <ergl@users.noreply.github.com>
2018-04-11 15:36:16 +0200

    Move ip transfer and clustering to prepare stage

---
+++ scripts/g5k/distribute-ips.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [[ $# -ne 1 ]]; then
+  exit 1
+fi
+
+# transferIPs <bench_file> <antidote_ip_file>
+# Transfer the antidote_ip_file to every node
+# in <bench_file>
+transferIPs () {
+  local bench_node_file="$1"
+  local antidote_ips_file="$2"
+  local antidote_ips_file_name=$(basename "${antidote_ips_file}")
+
+  while read node; do
+    scp -i "${EXPERIMENT_PRIVATE_KEY}" "${antidote_ips_file}" root@"${node}":/root/"${antidote_ips_file_name}"
+  done < "${bench_node_file}"
+}
+
+run() {
+  local total_nodes="${1}"
+  local antidote_ip_file=".antidote_ip_file"
+
+  # This assumes that each bench node only talks to one Antidote node
+  local ant_offset=0
+  local bench_offset=0
+  for _ in $(seq 1 ${total_nodes}); do
+    # Each bench node will receive one Antidote node IP to talk to
+    head -n $((ANTIDOTE_NODES + ant_offset)) "${ANT_IPS}" > "${antidote_ip_file}"
+    head -n $((BENCH_NODES + bench_offset)) "${BENCH_NODEF}" > .lasp_bench_nodes
+
+    transferIPs .lasp_bench_nodes "${antidote_ip_file}"
+
+    ant_offset=$((ant_offset + ANTIDOTE_NODES))
+    bench_offset=$((bench_offset + BENCH_NODES))
+  done
+}
+
+run "$@"
--- scripts/g5k/run-benchmark.sh
+++ scripts/g5k/run-benchmark.sh
@@ -2,42 +2,7 @@
 
 set -eo pipefail
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: ${0##/*} total-nodes"
-  exit 1
-fi
-
 source ./configuration.sh
-
-transferIPs () {
-  local bench_node_file="$1"
-  local antidote_ips_file="$2"
-  local antidote_ips_file_name=$(basename "${antidote_ips_file}")
-
-  local bench_dc_nodes=( $(< "${bench_node_file}") )
-  for node in "${bench_dc_nodes[@]}"; do
-    scp -i ${EXPERIMENT_PRIVATE_KEY} "${antidote_ips_file}" root@${node}:/root/${antidote_ips_file_name}
-  done
-}
-
-prepareTests () {
-  local total_nodes="${1}"
-  local antidote_ip_file="${2}"
-  # Prepare and join the Antidote cluster
-  ./prepare-clusters.sh "${total_nodes}"
-
-  local ant_offset=0
-  local bench_offset=0
-  for _ in $(seq 1 ${total_dcs}); do
-    head -$((ANTIDOTE_NODES + ant_offset)) "${ANT_IPS}" > "${antidote_ip_file}"
-    head -$((BENCH_NODES + bench_offset)) "${BENCH_NODEF}" > .dc_bench_nodes
-
-    transferIPs .dc_bench_nodes "${antidote_ip_file}"
-
-    ant_offset=$((ant_offset + ANTIDOTE_NODES))
-    bench_offset=$((bench_offset + BENCH_NODES))
-  done
-}
 
 runRemoteBenchmark () {
   local instances="$1"
@@ -54,14 +19,10 @@ runRemoteBenchmark () {
 }
 
 run () {
-  local total_nodes="${1}"
   local antidote_ip_file=".antidote_ip_file"
-
-  prepareTests "${total_nodes}" "${antidote_ip_file}"
-
   local bench_instances="${BENCH_INSTANCES}"
   local benchmark_configuration_file="${BENCH_FILE}"
   runRemoteBenchmark "${bench_instances}" "${benchmark_configuration_file}" "${antidote_ip_file}"
 }
 
-run "$@"
+run
