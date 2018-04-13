201648fda75b55dbc1f44520029bedb18f9b4546
ergl <ergl@users.noreply.github.com>
2018-04-13 14:10:56 +0200

    Deploy nancy and rennes

--- scripts/g5k/configuration.sh
+++ scripts/g5k/configuration.sh
@@ -9,7 +9,7 @@ PBKFILE=~/.ssh/benchmark.pub
 K3_IMAGE=/home/$(whoami)/public/antidote-images/latest/antidote.env
 
 # Different g5k sites to run the benchmark
-SITES=( "nancy" )
+SITES=( "nancy" "rennes" )
 
 # Reserve sites and nodes through oargridsub
 RESERVE_SITES=true
