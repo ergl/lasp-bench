3da70561bf52338874f64e08f8efa72cd0c3264e
ergl <ergl@users.noreply.github.com>
2018-04-18 18:46:24 +0200

    Follow current symlink

--- scripts/g5k/run-benchmark-remote.sh
+++ scripts/g5k/run-benchmark-remote.sh
@@ -13,8 +13,8 @@ runBench () {
 collectBench () {
   local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
   local result_name="results-${own_node_name}.tar"
-  pushd /root/lasp-bench > /dev/null 2>&1
-  tar -zcf "${result_name}" tests/2*
+  pushd /root/lasp-bench/tests > /dev/null 2>&1
+  tar -zhcf "${result_name}" current
   mv "${result_name}" /root
   popd > /dev/null 2>&1
 }
