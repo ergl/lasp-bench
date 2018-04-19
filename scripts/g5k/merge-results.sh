c2e440e9dbd071923bddbbe3594f2df760139ee1
ergl <ergl@users.noreply.github.com>
2018-04-19 10:26:35 +0200

    Merge results phase

--- scripts/g5k/merge-results.sh
+++ scripts/g5k/merge-results.sh
@@ -1,47 +1,40 @@
 #!/usr/bin/env bash
 
-# TODO
-# There should be read and update latencies, as well as a summary
-
-READ_PERCENTAGES=( 99 90 75 50 )
-
-processPercentage () {
-  local percentage="$1"
-
-  local analysis_files=( "summary.csv" "update-only-txn_latencies.csv" )
-  local percentage_files=( $(find . -type f -name "*${percentage}.tar"}) )
-
-  local -A file_mapping=()
-  mkdir -p summary-${percentage}
-  for file in "${percentage_files[@]}"; do
-    local dir_name="${file%.*}"
-    mkdir -p "${dir_name}"
-    tar -C "${dir_name}" -zzf "${file}"
-    local analysis_folder=$( ls -l "${dir_name}"/tests/current | awk -F "/" '{print $NF}' )
-    for analysis_file in "${analysis_files[@]}"; do
-      file_mapping["${analysis_file}"]+="${dir_name}/tests/${analysis_folder}/${analysis_file} "
-    done
-  done
-
-  # TODO: Don't hardcode this
-  for key in "${!file_mapping[@]}"; do
-    local awk_script=../basho_bench/script/mergeResults.awk
-    if [[ "$key" == "summary.csv" ]]; then
-      awk_script=../basho_bench/script/mergeResultsSummary.awk
-    fi
-    awk -f "${awk_script}" "${file_mapping[$key]}" > summary-${percentage}
-  done
-}
-
 run () {
   local test_directory="$1"
-  local latency_files=( "summary.csv" "update-only-txn_latencies.csv" )
+
+  # Move the processing scripts to the test directory
+  cp ../../priv/mergeLatencies.awk "${test_directory}"
+  cp ../../priv/mergeSummary.awk  "${test_directory}"
 
   pushd "${test_directory}" > /dev/null 2>&1
 
-  for percentage in "${READ_PERCENTAGES[@]}"; do
-    processPercentage "${percentage}"
+  local index=0
+  local -a result_folders
+  for result_tar in *.tar; do
+    # Remove .tar from file name
+    extract_to="${result_tar::-4}"
+    result_folders[${index}]="${extract_to}"
+    mkdir -p "${extract_to}"
+    # Extract the real contents of the tar to the extract_to folder
+    tar -xf "${result_tar}" -C "${extract_to}" --strip-components 1
+    index=$((index + 1))
   done
+
+  rm ./*.tar
+
+  local summary_str=""
+  local latency_str=""
+  for folder_name in "${result_folders[@]}"; do
+    summary_str+="${folder_name}summary.csv "
+    latency_str+="${folder_name}perform-operation_latencies.csv "
+  done
+
+  ./mergeSummary.awk "${summary_str}" > summary.csv
+  ./mergeLatencies.awk "${latency_str}" > perform-operation_latencies.csv
+
+  rm mergeSummary.awk
+  rm mergeLatencies.awk
 
   popd > /dev/null 2>&1
 }
