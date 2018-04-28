b61d0e6a90f0b726bb42f7b3c1dd4ef97e8d974d
ergl <ergl@users.noreply.github.com>
2018-04-28 12:48:39 +0200

    Format R script, don't plot max latencies

--- priv/summary.r
+++ priv/summary.r
@@ -51,27 +51,29 @@ if (nrow(b$latencies) == 0)
 png(file = opt$outfile, width = opt$width, height = opt$height)
 
 # First plot req/sec from summary
-plot1 <- qplot(elapsed, successful / window, data = b$summary,
-                geom = c("smooth", "point"),
-                xlab = "Elapsed Secs", ylab = opt$ylabel1stgraph,
-                main = opt$title) +
+thr <- qplot(elapsed,
+             successful / window,
+             data = b$summary,
+             geom = c("smooth", "point"),
+             xlab = "Time (secs)",
+             ylab = "Transactions / sec",
+             main = "Throughput") +
 
-                geom_smooth(aes(y = successful / window, colour = "ok"), size=0.5) +
-                geom_point(aes(y = successful / window, colour = "ok"), size=4.0) +
+        geom_smooth(aes(y = successful / window, colour = "Committed"), size=0.5) +
+        geom_point(aes(y = successful / window, colour = "Committed"), size=4.0) +
 
-                geom_smooth(aes(y = failed / window, colour = "error"), size=0.5) +
-                geom_point(aes(y = failed / window, colour = "error"), size=2.0) +
+        geom_smooth(aes(y = failed / window, colour = "Aborted"), size=0.5) +
+        geom_point(aes(y = failed / window, colour = "Aborted"), size=2.0) +
 
-                scale_colour_manual("Response", values = c("#FF665F", "#188125"))
-
+        scale_colour_manual("Response", values = c("#FF665F", "#188125"))
 
 # Setup common elements of the latency plots
 latency_plot <- ggplot(b$latencies, aes(x = elapsed)) +
                    facet_grid(. ~ op) +
-                   labs(x = "Elapsed Secs", y = "Latency (ms)")
+                   labs(x = "Time (secs)", y = "Latency (ms)")
 
 # Plot median, mean and 95th percentiles
-plot2 <- latency_plot + labs(title = "Mean, Median, and 95th Percentile Latency") +
+lat_all <- latency_plot + labs(title = "Mean, Median, and 95th Percentile Latency") +
             geom_smooth(aes(y = median, color = "median"), size=0.5) +
             geom_point(aes(y = median, color = "median"), size=2.0) +
 
@@ -86,37 +88,27 @@ plot2 <- latency_plot + labs(title = "Mean, Median, and 95th Percentile Latency"
             #                 breaks = c("X95th", "mean", "median"),
             #                 labels = c("95th", "Mean", "Median"))
 
-# Plot 99th percentile
-plot3 <- latency_plot + labs(title = "99th Percentile Latency") +
-            geom_smooth(aes(y = X99th, color = "99th"), size=0.5) +
-            geom_point(aes(y = X99th, color = "99th"), size=2.0) +
-            scale_colour_manual("Percentile", values = c("#FF665F", "#009D91"))
-            # scale_color_hue("Percentile",
-            #                 breaks = c("X99_9th","X99th" ),
-            #                 labels = c("99.9th", "99th"))
-
 # Plot 99.9th percentile
-plot4 <- latency_plot + labs(title = "99.9th Percentile Latency") +
+lat_999 <- latency_plot + labs(title = "99.9th Percentile Latency") +
             geom_smooth(aes(y = X99_9th, color = "99.9th"), size=0.5) +
             geom_point(aes(y = X99_9th, color = "99.9th"), size=2.0) +
             scale_colour_manual("Percentile", values = c("#FF665F", "#009D91", "#FFA700"))
 
 # Plot 100th percentile
-plot5 <- latency_plot + labs(title = "Maximum Latency") +
+lat_max <- latency_plot + labs(title = "Maximum Latency") +
             geom_smooth(aes(y = max, color = "max"), size=0.5) +
             geom_point(aes(y = max, color = "max"), size=2.0) +
             scale_colour_manual("Percentile", values = c("#FF665F", "#009D91", "#FFA700"))
 
 grid.newpage()
 
-pushViewport(viewport(layout = grid.layout(5, 1)))
+pushViewport(viewport(layout = grid.layout(3, 1)))
 
 vplayout <- function(x,y) viewport(layout.pos.row = x, layout.pos.col = y)
 
-print(plot1, vp = vplayout(1,1))
-print(plot2, vp = vplayout(2,1))
-print(plot3, vp = vplayout(3,1))
-print(plot4, vp = vplayout(4,1))
-print(plot5, vp = vplayout(5,1))
+print(thr, vp = vplayout(1,1))
+print(lat_all, vp = vplayout(2,1))
+print(lat_999, vp = vplayout(3,1))
+#print(lat_max, vp = vplayout(4,1))
 
 dev.off()
