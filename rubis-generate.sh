d983652364ef910d92192d44e9f44f7549e9b066
ergl <ergl@users.noreply.github.com>
2018-04-29 16:06:12 +0200

    Add back option to follow default transitions

--- rubis-generate.sh
+++ rubis-generate.sh
@@ -6,8 +6,13 @@ generate() {
   local out_file="$(pwd)/${3}"
   pushd scripts/
   local load_file="$(pwd)"/output.json
+  local ops_file="$(pwd)"/default_ops.json
   if [[ -a "${load_file}" ]]; then
-    ./genbenchrun.escript "${host}" "${port}" "${load_file}" > "${out_file}"
+    if [[ -a "${ops_file}" ]]; then
+      ./genbenchrun.escript "${host}" "${port}" "${load_file}" "${ops_file}" > "${out_file}"
+    else
+      ./genbenchrun.escript "${host}" "${port}" "${load_file}" > "${out_file}"
+    fi
   else
     popd
     exit 1
