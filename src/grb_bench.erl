9d87716f92c0e99dc2711c062ea7b4a483687c17
ergl <ergl@users.noreply.github.com>
2020-06-17 15:59:13 +0200

    Add GRB blue benchmark

---
+++ src/grb_bench.erl
@@ -0,0 +1,134 @@
+-module(grb_bench).
+
+%% Ignore xref for all lasp bench drivers
+-ignore_xref([new/1, run/4]).
+
+-export([new/1,
+         run/4]).
+
+-include("lasp_bench.hrl").
+
+-type tx_id() :: {non_neg_integer(), non_neg_integer()}.
+-type key_gen(T) :: fun(() -> T).
+
+-record(state, {
+    %% Id of this worker thread
+    worker_id :: non_neg_integer(),
+    %% Auto incremented counter to create unique transaction ids
+    transaction_count :: non_neg_integer(),
+    %% Client coordinator state
+    coord_state :: pvc:coord_state(),
+    readonly_ops :: non_neg_integer(),
+    writeonly_ops :: non_neg_integer(),
+    mixed_ops_ration :: {non_neg_integer(), non_neg_integer()},
+    keep_cvc :: boolean(),
+    last_cvc :: pvc_vclock:vc(),
+    retry_until_commit :: boolean()
+}).
+
+new(Id) ->
+    RonlyOps = lasp_bench_config:get(readonly_ops),
+    WonlyOps = lasp_bench_config:get(writeonly_ops),
+    MixedOpsRatio = lasp_bench_config:get(ratio),
+    KeepCVC = lasp_bench_config:get(keep_cvc),
+    RetryUntilCommit = lasp_bench_config:get(retry_aborts, true),
+
+    ReplicaID = ets:lookup_element(hook_grb, replica_id, 2),
+    RingInfo = ets:lookup_element(hook_grb, ring, 2),
+    Connections = hook_pvc:conns_for_worker(Id),
+
+    {ok, CoordState} = pvc:grb_new(RingInfo, Connections, Id, ReplicaID),
+    State = #state{worker_id=Id,
+                   transaction_count=0,
+                   coord_state=CoordState,
+                   readonly_ops=RonlyOps,
+                   writeonly_ops=WonlyOps,
+                   mixed_ops_ration=MixedOpsRatio,
+                   keep_cvc=KeepCVC,
+                   last_cvc=pvc_vclock:new(),
+                   retry_until_commit=RetryUntilCommit},
+
+    {ok, State}.
+
+run(readonly_blue, KeyGen, _, State = #state{readonly_ops=N}) ->
+    Keys = gen_keys(N, KeyGen),
+    perform_readonly_blue(Keys, State);
+
+run(writeonly_blue, KeyGen, ValueGen, State = #state{writeonly_ops=N}) ->
+    Keys = gen_keys(N, KeyGen),
+    Ops = [ {K, ValueGen()} || K <- Keys],
+    perform_writeonly_blue(Ops, State);
+
+run(read_write_blue, KeyGen, ValueGen, State = #state{mixed_ops_ration={RN, WN}}) ->
+    ReadKeys = gen_keys(RN, KeyGen),
+    WriteKeys = gen_keys(WN, KeyGen),
+    Updates = [ {K, ValueGen()} || K <- WriteKeys],
+    perform_read_write_blue(ReadKeys, Updates, State).
+
+perform_readonly_blue(Keys, State=#state{coord_state=CoordState}) ->
+    {ok, Tx} = maybe_start_with_clock(State),
+    %% todo(borja): Implement multi-key read
+    Tx1 = lists:foldl(fun(K, AccTx) ->
+        {ok, _, NextTx} = pvc:grb_ronly_op(CoordState, AccTx, K),
+        NextTx
+    end, Tx, Keys),
+    CVC = pvc:grb_blue_commit(CoordState, Tx1),
+    {ok, maybe_keep_clock(State, CVC)}.
+
+perform_writeonly_blue(Updates, State=#state{coord_state=CoordState}) ->
+    {ok, Tx} = maybe_start_with_clock(State),
+    %% todo(borja): Implement multi-key update
+    Tx1 = lists:foldl(fun({K, V}, AccTx) ->
+        {ok, _, NextTx} = pvc:grb_op(CoordState, AccTx, K, V),
+        NextTx
+    end, Tx, Updates),
+    CVC = pvc:grb_blue_commit(CoordState, Tx1),
+    {ok, maybe_keep_clock(State, CVC)}.
+
+perform_read_write_blue(Keys, Updates, State=#state{coord_state=CoordState}) ->
+    {ok, Tx} = maybe_start_with_clock(State),
+    %% todo(borja): Implement multi-key read and update
+    Tx1 = lists:foldl(fun(K, AccTx) ->
+        {ok, _, NextTx} = pvc:grb_ronly_op(CoordState, AccTx, K),
+        NextTx
+    end, Tx, Keys),
+
+    Tx2 = lists:foldl(fun({K, V}, AccTx) ->
+        {ok, _, NextTx} = pvc:grb_op(CoordState, AccTx, K, V),
+        NextTx
+    end, Tx1, Updates),
+    CVC = pvc:grb_blue_commit(CoordState, Tx2),
+    {ok, maybe_keep_clock(State, CVC)}.
+
+%%====================================================================
+%% Util functions
+%%====================================================================
+
+maybe_start_with_clock(S=#state{coord_state=CoordState, keep_cvc=true, last_cvc=VC}) ->
+    pvc:grb_start_tx(CoordState, next_tx_id(S), VC);
+maybe_start_with_clock(S=#state{coord_state=CoordState, keep_cvc=false}) ->
+    pvc:grb_start_tx(CoordState, next_tx_id(S)).
+
+maybe_keep_clock(S=#state{keep_cvc=true}, CVC) ->
+    incr_tx_id(S#state{last_cvc=CVC});
+maybe_keep_clock(S=#state{keep_cvc=false}, _) ->
+    incr_tx_id(S).
+
+%% @doc Generate N random keys
+-spec gen_keys(non_neg_integer(), key_gen(non_neg_integer())) -> binary() | [binary()].
+gen_keys(N, K) ->
+    gen_keys(N, K, []).
+
+gen_keys(0, _, Acc) ->
+    Acc;
+
+gen_keys(N, K, Acc) ->
+    gen_keys(N - 1, K, [integer_to_binary(K(), 36) | Acc]).
+
+-spec next_tx_id(#state{}) -> tx_id().
+next_tx_id(#state{worker_id=Id, transaction_count=N}) ->
+    {Id, N}.
+
+-spec incr_tx_id(#state{}) -> #state{}.
+incr_tx_id(State=#state{transaction_count=N}) ->
+    State#state{transaction_count = N + 1}.
---
+++ src/hook_grb.erl
@@ -0,0 +1,107 @@
+-module(hook_grb).
+
+-ignore_xref([start/1, stop/0]).
+
+%% API
+-export([start/1,
+         stop/0]).
+
+%% Worker API
+-export([conns_for_worker/1]).
+
+%% Get ring information from Antidote,
+%% and spawn all the necessary connections
+start(HookOpts) ->
+    logger:info("~p:~p(~p)", [?MODULE, ?FUNCTION_NAME, HookOpts]),
+    ok = pvc:start(),
+
+    %% ETS table to share data with workers
+    _ = ets:new(?MODULE, [set, named_table, protected]),
+
+    NodeNameOpt = lists:keyfind(bootstrap_node, 1, HookOpts),
+    NodeIPOpt = lists:keyfind(bootstrap_node_ip, 1, HookOpts),
+    NodeClusterOpt = lists:keyfind(bootstrap_cluster, 1, HookOpts),
+
+    {bootstrap_port, Port} = lists:keyfind(bootstrap_port, 1, HookOpts),
+    {conn_pool_size, PoolSize} = lists:keyfind(conn_pool_size, 1, HookOpts),
+    {connection_port, ConnectionPort} = lists:keyfind(connection_port, 1, HookOpts),
+
+    BootstrapIp = get_bootstrap_ip(NodeNameOpt, NodeClusterOpt, NodeIPOpt),
+    logger:info("Given bootstrap IP ~p~n", [BootstrapIp]),
+
+    ConnectionOpts0 = case lists:keyfind(connection_cork_ms, 1, HookOpts) of
+        {connection_cork_ms, CorkMs} -> #{cork_len => CorkMs};
+        false -> #{}
+    end,
+
+    ConnectionOpts1 = case lists:keyfind(connection_buff_wm, 1, HookOpts) of
+        {connection_buff_wm, BuffWatermark} ->
+            maps:put(buf_watermark, BuffWatermark, ConnectionOpts0);
+        false ->
+            ConnectionOpts0
+    end,
+
+    {ok, ReplicaID, Ring, UniqueNodes} = pvc_ring:grb_replica_info(BootstrapIp, Port),
+    ets:insert(?MODULE, {replica_id, ReplicaID}),
+    ets:insert(?MODULE, {ring, Ring}),
+    ets:insert(?MODULE, {nodes, UniqueNodes}),
+    ok = lists:foreach(fun(NodeIp) ->
+        Connections = spawn_pool(NodeIp, ConnectionPort, ConnectionOpts1, PoolSize),
+        ets:insert(?MODULE, {NodeIp, PoolSize, Connections}),
+        ok
+    end, UniqueNodes).
+
+stop() ->
+    logger:info("Unloading ~p", [?MODULE]),
+    [{nodes, UniqueNodes}] = ets:take(?MODULE, nodes),
+    lists:foreach(fun(NodeIp) ->
+        teardown_pool(NodeIp)
+    end, UniqueNodes),
+    ets:delete(?MODULE),
+    ok = pvc:stop(),
+    ok.
+
+-spec conns_for_worker(non_neg_integer()) -> pvc:cluster_conns().
+conns_for_worker(WorkerId) ->
+    Nodes = ets:lookup_element(?MODULE, nodes, 2),
+    [begin
+         [{Node, PoolSize, Connections}] = ets:lookup(?MODULE, Node),
+         {Node, lists:nth((WorkerId rem PoolSize) + 1, Connections)}
+     end || Node <- Nodes].
+
+%% If Node IP option is present, override anything else
+-spec get_bootstrap_ip(tuple() | false, tuple() | false, tuple() | false) -> atom().
+get_bootstrap_ip(_, _, {bootstrap_node_ip, NodeIp}) ->
+    NodeIp;
+
+%% If node name is given, and we're on Emulab, parse /etc/hosts to get node IP
+get_bootstrap_ip({bootstrap_node, NodeName}, {bootstrap_cluster, emulab}, _) ->
+    NameB = atom_to_binary(NodeName, latin1),
+    {ok, Raw} = file:read_file("/etc/hosts"),
+    Lines = binary:split(Raw, <<"\n">>, [global, trim_all]),
+    [BootstrapIp] = [begin
+                         [IP | _] = binary:split(L, <<"\t">>),
+                         binary_to_atom(IP, latin1)
+                     end || L <- Lines, binary:match(L, NameB) =/= nomatch],
+    BootstrapIp;
+
+%% If we're on Apollo, just use inet
+get_bootstrap_ip({bootstrap_node, NodeName}, {bootstrap_cluster, apollo}, _) ->
+    {ok, Addr} = inet:getaddr(NodeName, inet),
+    list_to_atom(inet:ntoa(Addr)).
+
+%% Create a pool of connections to `NodeIp:ConnectionPort` of size `PoolSize`
+spawn_pool(NodeIp, ConnectionPort, ConnectionOpts, PoolSize) ->
+    spawn_pool(NodeIp, ConnectionPort, ConnectionOpts, PoolSize, []).
+
+spawn_pool(_, _, _, 0, Acc) ->
+    Acc;
+
+spawn_pool(Ip, Port, Opts, N, Acc) ->
+    {ok, Connection} = pvc_connection:new(Ip, Port, Opts),
+    spawn_pool(Ip, Port, Opts, N - 1, [Connection | Acc]).
+
+%% Close all the pvc connections
+teardown_pool(NodeIp) ->
+    [{NodeIp, _, Connections}] = ets:take(?MODULE, NodeIp),
+    lists:foreach(fun pvc_connection:close/1, Connections).
