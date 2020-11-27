d3964c01f7ca0d8d74da59e10ca1c69166bba122
ergl <ergl@users.noreply.github.com>
2020-11-27 11:59:27 +0100

    Clean up rubis files

--- src/rubis_bench.erl
+++ src/rubis_bench.erl
@@ -4,258 +4,59 @@
 -ignore_xref([new/1, run/4]).
 
 -export([new/1,
-         run/4]).
+         run/4,
+         terminate/2]).
 
 -include("lasp_bench.hrl").
 
 -record(state, {
-    %% Antidote connection to the remote node
-    worker_id,
-    socket,
-    rubis_state
+    worker_id :: non_neg_integer(),
+    transaction_count :: non_neg_integer(),
+    last_cvc :: pvc_vclock:vc(),
+    retry_until_commit :: boolean(),
+    coord_state :: grb_client:coord(),
+    rubis_state :: rubis_bench_utils:t()
 }).
 
-new(Id) ->
-    _ = application:ensure_all_started(pvc_proto),
+new(WorkerId) ->
+    {ok, CoordState} = hook_grb:make_coordinator(WorkerId),
 
-    Ip = lasp_bench_config:get(rubis_ip, '127.0.0.1'),
-    Port = lasp_bench_config:get(rubis_port, 7878),
-    Options = [binary, {active, false}, {packet, 2}],
-    {ok, Sock} = gen_tcp:connect(Ip, Port, Options),
+    RubisState = rubis_bench_utils:new(),
+    RetryUntilCommit = lasp_bench_config:get(retry_aborts, true),
 
-    RubisState = rubis_bench_core:new_rubis_state(),
-    {ok, #state{socket=Sock,
-                worker_id=Id,
-                rubis_state=RubisState}}.
+    RandSeed = lasp_bench_config:get(worker_seed, os:timestamp()),
+    _ = rand:seed(exsss, RandSeed),
 
-run(perform_operation, _, _, State = #state{rubis_state=RState}) ->
-    {Operation, NewRState} = rubis_bench_core:next_operation(RState),
-    Resp = run_op(Operation, State#state{rubis_state=NewRState}),
-    sleep_then_return(Operation, Resp).
+    State = #state{worker_id=WorkerId,
+                   transaction_count=0,
+                   last_cvc=pvc_vclock:new(),
+                   retry_until_commit=RetryUntilCommit,
+                   coord_state=CoordState,
+                   rubis_state=RubisState},
 
-run_op(registeruser, State = #state{socket=Sock, rubis_state=RState}) ->
-    RegionId = rubis_bench_core:random_region_id(RState),
-    {{Username, Password}, NewState} = rubis_bench_core:gen_new_user(RState),
-    Msg = ppb_rubis_driver:register_user(Username, Password, RegionId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, Id} ->
-            FinalState = rubis_bench_core:update_state({confirm_user, Id, Username}, NewState),
-            {ok, State#state{rubis_state=FinalState}};
+    {ok, State}.
 
-         {error, Reason} ->
-             FinalState = rubis_bench_core:update_state({discard_user, Username}, NewState),
-             {error, Reason, State#state{rubis_state=FinalState}}
-    end;
+run(register_user, _, _, S) -> {ok, S};
+run(browse_categories, _, _, S) -> {ok, S};
+run(search_items_in_category, _, _, S) -> {ok, S};
+run(browse_regions, _, _, S) -> {ok, S};
+run(browse_categories_in_region, _, _, S) -> {ok, S};
+run(search_items_in_region, _, _, S) -> {ok, S};
+run(view_item, _, _, S) -> {ok, S};
+run(view_user_info, _, _, S) -> {ok, S};
+run(view_bid_history, _, _, S) -> {ok, S};
+run(buy_now, _, _, S) -> {ok, S};
+run(store_buy_now, _, _, S) -> {ok, S};
+run(put_bid, _, _, S) -> {ok, S};
+run(store_bid, _, _, S) -> {ok, S};
+run(put_comment, _, _, S) -> {ok, S};
+run(store_comment, _, _, S) -> {ok, S};
+run(select_category_to_sell_item, _, _, S) -> {ok, S};
+run(register_item, _, _, S) -> {ok, S};
+run(about_me, _, _, S) -> {ok, S};
+run(get_auctions_ready_for_close, _, _, S) -> {ok, S};
+run(close_auction, _, _, S) -> {ok, S}.
 
-run_op(browsecategories, State = #state{socket=Sock}) ->
-    Msg = ppb_rubis_driver:browse_categories(),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(searchitemsincategory, State = #state{socket=Sock, rubis_state=RState}) ->
-    CategoryId = rubis_bench_core:random_category_id(RState),
-    Msg = ppb_rubis_driver:search_items_by_category(CategoryId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(browseregions, State = #state{socket=Sock}) ->
-    Msg = ppb_rubis_driver:browse_regions(),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(searchitemsinregion, State = #state{socket=Sock, rubis_state=RState}) ->
-    RegionId = rubis_bench_core:random_region_id(RState),
-    CategoryId = rubis_bench_core:random_category_id(RState),
-    Msg = ppb_rubis_driver:search_items_by_region(CategoryId, RegionId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(viewitem, State = #state{socket=Sock, rubis_state=RState}) ->
-    ItemId = rubis_bench_core:random_item_id(RState),
-    Msg = ppb_rubis_driver:view_item(ItemId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(viewuserinfo, State = #state{socket=Sock, rubis_state=RState}) ->
-    UserId = rubis_bench_core:random_user_id(RState),
-    Msg = ppb_rubis_driver:view_user(UserId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(viewbidhistory, State = #state{socket=Sock, rubis_state=RState}) ->
-    ItemId = rubis_bench_core:random_item_id(RState),
-    Msg = ppb_rubis_driver:view_bid_history(ItemId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(buynowauth, State) ->
-    perform_auth(State);
-
-run_op(storebuynow, State = #state{socket=Sock,rubis_state=RState}) ->
-    {ItemId, UserId, Q} = rubis_bench_core:gen_buy_now(RState),
-    Msg = ppb_rubis_driver:store_buy_now(ItemId, UserId, Q),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, _} ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(putbidauth, State) ->
-    perform_auth(State);
-
-run_op(storebid, State = #state{socket=Sock,rubis_state=RState}) ->
-    {ItemId, UserId, Price} = rubis_bench_core:gen_bid(RState),
-    Msg = ppb_rubis_driver:store_bid(ItemId, UserId, Price),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, _} ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(putcommentauth, State) ->
-    perform_auth(State);
-
-run_op(storecomment, State = #state{socket=Sock,rubis_state=RState}) ->
-    {ItemId, FromId, ToId, Rating, Body} = rubis_bench_core:gen_comment(RState),
-    Msg = ppb_rubis_driver:store_comment(ItemId, FromId, ToId, Rating, Body),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, _} ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(registeritem, State = #state{socket=Sock, rubis_state=RState}) ->
-    {ItemName, Description, Quantity, CategoryId, SellerId} = rubis_bench_core:gen_item(RState),
-    Msg = ppb_rubis_driver:store_item(ItemName, Description, Quantity, CategoryId, SellerId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, ItemId} ->
-            NewState = rubis_bench_core:update_state({confirm_item, ItemId}, RState),
-            {ok, State#state{rubis_state=NewState}};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end;
-
-run_op(aboutme_auth, State) ->
-    perform_auth(State);
-
-run_op(aboutme, State = #state{socket=Sock, rubis_state=RState}) ->
-    UserId = rubis_bench_core:random_user_id(RState),
-    Msg = ppb_rubis_driver:about_me(UserId),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        ok ->
-            {ok, State};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end.
-
-
-%% Util functions
-
--spec sleep_then_return(atom(), Resp) -> Resp.
-sleep_then_return(Operation, Resp) ->
-    Rstate = get_state(Resp),
-    ok = rubis_bench_core:wait_time(Operation, Rstate),
-    Resp.
-
-get_state({ok, #state{rubis_state=RState}}) ->
-    RState;
-
-get_state({error, _, #state{rubis_state=RState}}) ->
-    RState.
-
-perform_auth(State = #state{socket=Sock, rubis_state=RState}) ->
-    {Username, Pass} = rubis_bench_core:random_user(RState),
-    Msg = ppb_rubis_driver:auth_user(Username, Pass),
-    ok = gen_tcp:send(Sock, Msg),
-    {ok, BinReply} = gen_tcp:recv(Sock, 0),
-    Resp = pvc_proto:decode_serv_reply(BinReply),
-    case Resp of
-        {ok, Id} ->
-            NewState = rubis_bench_core:set_logged_in(Id, RState),
-            {ok, State#state{rubis_state=NewState}};
-
-        {error, Reason} ->
-            {error, Reason, State}
-    end.
+terminate(_Reason, _State) ->
+    hook_grb:stop(),
+    ok.
---
+++ src/rubis_bench_utils.erl
@@ -0,0 +1,98 @@
+-module(rubis_bench_utils).
+
+-record(state, {
+    %% todo(borja, rubis): Types?
+    session_id :: term(),
+    session_username :: term(),
+    session_password :: term(),
+
+    %% todo(borja, rubis): Rethink this (pregenerated ids, props in config, etc)
+    region_ids :: tuple(),
+    region_ids_len :: non_neg_integer(),
+
+    category_ids :: tuple(),
+    category_ids_len :: non_neg_integer(),
+
+    user_ids :: #{non_neg_integer() => term()},
+    user_ids_len :: non_neg_integer(),
+
+    item_ids :: #{non_neg_integer() => term()},
+    item_ids_len :: non_neg_integer(),
+
+    usernames :: #{non_neg_integer() := binary()},
+    usernames_len :: non_neg_integer()
+}).
+
+-type t() :: #state{}.
+-export_type([t/0]).
+%% State API
+-export([new/0,
+         random_region_id/1,
+         random_category_id/1,
+         random_user_id/1,
+         different_user_id/2,
+         random_item_id/1,
+         random_user/1]).
+
+%% Util API
+-export([random_binary/1,
+         random_email/1,
+         random_rating/0,
+         random_boolean/0]).
+
+-spec new() -> t().
+new() ->
+    #state{}.
+
+-spec random_region_id(t()) -> term().
+random_region_id(#state{region_ids=Regions, region_ids_len=Len}) ->
+    erlang:element(rand:uniform(Len), Regions).
+
+-spec random_category_id(t()) -> term().
+random_category_id(#state{category_ids=Categories, category_ids_len=Len}) ->
+    erlang:element(rand:uniform(Len), Categories).
+
+-spec random_user_id(t()) -> term().
+random_user_id(#state{user_ids=Users,user_ids_len=Len}) ->
+    maps:get(rand:uniform(Len), Users).
+
+-spec different_user_id(term(), t()) -> term().
+different_user_id(Id, S) ->
+    different_user_id(Id, random_user_id(S), S).
+
+different_user_id(Id, Id, S) -> different_user_id(Id, random_user_id(S), S);
+different_user_id(_Id, Id2, _S) -> Id2.
+
+-spec random_item_id(t()) -> term().
+random_item_id(#state{item_ids=Items,item_ids_len=Len}) ->
+    maps:get(rand:uniform(Len), Items).
+
+-spec random_user(t()) -> {UserName :: binary(), Password :: binary()}.
+random_user(#state{usernames = Usernames, usernames_len = Len}) ->
+    Username = maps:get(rand:uniform(Len), Usernames),
+    {Username, Username}.
+
+%% Utils
+
+-spec random_binary(Size :: non_neg_integer()) -> binary().
+random_binary(N) ->
+    list_to_binary(random_string_(N)).
+
+-spec random_email(Size :: non_neg_integer()) -> binary().
+random_email(N) ->
+    list_to_binary(random_string_(N) ++ "@example.com").
+
+-spec random_rating() -> non_neg_integer().
+random_rating() ->
+    rand:uniform(11) - 6.
+
+-spec random_boolean() -> boolean().
+random_boolean() ->
+    (rand:uniform(16) > 8).
+
+-spec random_string_(Size :: non_neg_integer()) -> list().
+random_string_(N) ->
+    Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890",
+    lists:foldl(fun(_, Acc) ->
+        [lists:nth(rand:uniform(length(Chars)), Chars)] ++ Acc
+    end, [], lists:seq(1, N)).
