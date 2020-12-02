3b20a349f8573d550d1e16d642bca07bc59d4f30
ergl <ergl@users.noreply.github.com>
2020-12-02 12:29:49 +0100

    Add config, persist in persistent_term to avoid copying

--- src/hook_rubis.erl
+++ src/hook_rubis.erl
@@ -12,7 +12,6 @@
 
 start(Args) ->
     logger:info("~p:~p(~p)", [?MODULE, ?FUNCTION_NAME, Args]),
-    _ = ets:new(?MODULE, [set, named_table, protected]),
 
     RubisPath = proplists:get_value(rubis_props_path, Args),
     logger:info("Loading properties from~s~n", [RubisPath]),
@@ -23,14 +22,14 @@ start(Args) ->
         case Key of
             regions ->
                 RT = list_to_tuple(Value),
-                true = ets:insert(?MODULE, {regions, RT}),
-                true = ets:insert(?MODULE, {regions_size, erlang:size(RT)});
+                ok = persistent_term:put({?MODULE, regions}, RT),
+                ok = persistent_term:put({?MODULE, regions_size}, erlang:size(RT));
             categories ->
                 CT = list_to_tuple(Value),
-                true = ets:insert(?MODULE, {categories, CT}),
-                true = ets:insert(?MODULE, {categories_size, erlang:size(CT)});
+                ok = persistent_term:put({?MODULE, categories}, CT),
+                ok = persistent_term:put({?MODULE, categories_size}, erlang:size(CT));
             _ ->
-                true = ets:insert(?MODULE, {Key, Value})
+                ok = persistent_term:put({?MODULE, Key}, Value)
         end,
         ok
     end, ok, RubisPropsMap),
@@ -44,7 +43,7 @@ get_config(Key) ->
     hook_grb:get_config(Key).
 
 get_rubis_prop(Key) ->
-    ets:lookup_element(?MODULE, Key, 2).
+    persistent_term:get({?MODULE, Key}).
 
 make_coordinator(Id) ->
     hook_grb:make_coordinator(Id).
